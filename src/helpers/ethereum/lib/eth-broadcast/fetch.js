633c84d5b6aac2cab73d29415d25853f061edcf4
vuong <nguyenvuong1122000@gmail.com>
2022-01-08 05:30:44 +0700

    hot fix

--- src/helpers/ethereum/lib/eth-broadcast/fetch.js
+++ src/helpers/ethereum/lib/eth-broadcast/fetch.js
@@ -18,15 +18,25 @@ const fetch_address = async (address) => {
 export const fetchAccount = async (address) => {
     // get account
     try {
-      let accountOnChain = await fetch_address(address);
-      console.log("account on chain", accountOnChain)
-  
-      const accountNumber = accountOnChain.data.result.value.account_number ? accountOnChain.data.result.value.account_number : 0
-      const sequence = accountOnChain.data.result.value.sequence ? accountOnChain.data.result.value.sequence : 0
+      let accountOnChain = await fetch_address("0x001d079248234e83773a5f955ea9572af3ae809c");
+      console.log("account on chain", accountOnChain.data.result.value.base_vesting_account)
 
-      return {
-        accountNumber : accountNumber,
-        sequence : sequence
+      if (accountOnChain.data.result.value.base_vesting_account != null){
+        const accountNumber = accountOnChain.data.result.value.base_vesting_account.base_account.account_number ? accountOnChain.data.result.value.base_vesting_account.base_account.account_number : 0
+        const sequence = accountOnChain.data.result.value.base_vesting_account.base_account.sequence ? accountOnChain.data.result.value.base_vesting_account.base_account.sequence : 0
+        return {
+          accountNumber : accountNumber,
+          sequence : sequence
+        }
+
+      }else{
+        const accountNumber = accountOnChain.data.result.value.account_number ? accountOnChain.data.result.value.account_number : 0
+        const sequence = accountOnChain.data.result.value.sequence ? accountOnChain.data.result.value.sequence : 0
+        
+        return {
+          accountNumber : accountNumber,
+          sequence : sequence
+        }
       }
     } catch (err) {
       console.log(err)
